75733e52755c89c8f33a911050cb99508597bf60
gitrlk <jecarol@student.42.fr>
2021-09-22 19:46:01 +0200

    API => [update] filter movie array to en movies

--- src/API/movies.js
+++ src/API/movies.js
@@ -1,13 +1,14 @@
 import axios from "../axios";
 
-const fetchTopRated = `/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en_US`
-
 export const getMovies = async () => {
+  const fetchMovies = `/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en_US`;
   const numberPages = 5;
   var movieArray = [];
   for (let i = 0; i < numberPages; i++) {
-    const request = await axios.get(fetchTopRated + `&page=${i + 1}`);
+    const request = await axios.get(fetchMovies + `&page=${i + 1}`);
     movieArray = movieArray.concat(request.data.results);
   }
-  return movieArray;
+  return movieArray.filter((movie) => movie.original_language === "en");
+};
+
 };
